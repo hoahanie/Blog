c9eadd286c57a32f7fd1f224c0c06c1efcea70e0
hoale <hoale160799@gmail.com>
2023-10-22 22:48:57 +0700

    Reset password

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,3 +137,9 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
+# EMAIL_BACKEN = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
